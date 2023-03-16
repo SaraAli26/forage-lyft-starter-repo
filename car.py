53b84ff1320145a62ea4e0de4b240990d8ee0208
Sara Ahmed <saraahmed2693@gmail.com>
2023-03-16 19:26:51 +0200

    The company's refactor

--- car.py
+++ car.py
@@ -1,13 +1,10 @@
-from abc import ABC, abstractmethod
 from serviceable import Serviceable
 
 
 class Car(Serviceable):
-    def __init__(self, last_service_date, battery, engine):
+    def __init__(self, battery, engine):
         self.battery = battery
         self.engine = engine
-        self.last_service_date = last_service_date
 
-    @abstractmethod
     def needs_service(self):
         pass
--- model/car_factory.py
+++
@@ -1,5 +0,0 @@
-
-from model.calliope import Calliope
-
-class CarFactory:
-    def CreateCar(self,type,last_Service_date,current_mileage):
